90404e55e30db4659ee0f004cab33f208e63177c
Dirk Eddelbuettel <edd@debian.org>
2021-03-19 19:17:04 -0500

    install nanotime (as its dependencies) as Debian binaries

--- scripts/ciRel.sh
+++ scripts/ciRel.sh
@@ -12,13 +12,13 @@ PKG_TGZ="${PKG_NAME}_${PKG_VER}.tar.gz"
 ## -- Update packages and install build dependencies
 apt update -qq
 apt upgrade -q -y
-apt install -y --no-install-recommends r-cran-rcpp r-cran-matrix r-cran-lattice r-cran-testthat r-cran-bit64 r-cran-xts
+apt install -y --no-install-recommends r-cran-rcpp r-cran-matrix r-cran-lattice r-cran-testthat r-cran-bit64 r-cran-xts r-cran-nanotime
 
 ## DelayedArray and S4Vectors in R are too old
 install.r BiocManager
 installBioc.r DelayedArray
 
-## -- install tiledb (plus remaining R package dependencies from nanotime)
+## -- install tiledb
 install.r tiledb
 
 ## -- now that everything is prepared, create a tarball and check it
